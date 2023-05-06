8184382abb1b9f7b4e3c1620ec1e129d17e18c9d
LuizFarabello <luizgufamartins@gmail.com>
2023-05-06 20:51:07 -0300

    fix: remove useless code

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
 import { Message } from './Message.jsx';
 
 export const App = () => (
--- server/main.js
+++ server/main.js
@@ -1,10 +1,5 @@
 import { Meteor } from "meteor/meteor";
-import { LinksCollection } from "/imports/api/links";
 import { MessagesCollection } from "../imports/api/messages";
-
-// async function insertLink({ title, url }) {
-//   await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
-// }
 
 Meteor.methods({
   "message.insert"(messageText) {
@@ -20,28 +15,6 @@ const insertMessage = (messageText) =>
   });
 
 Meteor.startup(async () => {
-  // If the Links collection is empty, add some data.
-  // if ((await MessagesCollection.find().countAsync()) === 0) {
-  //   await insertMessage("first message");
-
-  //   // await insertMessage({
-  //   //   title: 'Follow the Guide',
-  //   //   url: 'https://guide.meteor.com',
-  //   // });
-
-  //   // await insertMessage({
-  //   //   title: 'Read the Docs',
-  //   //   url: 'https://docs.meteor.com',
-  //   // });
-
-  //   // await insertMessage({
-  //   //   title: 'Discussions',
-  //   //   url: 'https://forums.meteor.com',
-  //   // });
-  // }
-
-  // We publish the entire Links collection to all clients.
-  // In order to be fetched in real-time to the clients
   Meteor.publish("messages", function () {
     return MessagesCollection.find();
   });
